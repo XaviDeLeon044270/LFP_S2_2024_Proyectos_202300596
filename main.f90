f10705fbfccb93c5571129b2e1ad2b56dbe1d68d
Chaval044270 <3033373110108@ingenieria.usac.edu.gt>
2024-09-21 22:53:00 -0600

    sexto commit

--- main.f90
+++ main.f90
@@ -1,6 +1,6 @@
 program main
     implicit none
-    integer :: i, len, linea, columna, estado, puntero, numErrores, numTokens, numPaises, numPaises_continente, numContinentes, ios, espacio_texto
+    integer :: i, len, linea, columna, estado, puntero, numErrores, numTokens, numPaises, numPaises_continente, numContinentes, ios, espacio_texto, suma_saturacion
     character(len=10000) :: contenido, buffer
     character(len=10) :: str_codigo_ascii, str_columna, str_linea
     character(len=1) :: char
@@ -17,15 +17,15 @@ program main
     logical :: is_pais, is_continente, is_nombre, is_poblacion, is_saturacion, is_bandera, nuevo_pais, nuevo_continente
 
     type :: ErrorInfo
-        character(len=10) :: caracter  ! caracter
-        character(len=100) :: descripcion  ! Descripción del error
+        character(len=100) :: caracter  ! caracter
+        character(len=50) :: descripcion  ! Descripción del error
         integer :: columna      ! Columna donde ocurrió el error
         integer :: linea        ! Línea donde ocurrió el error
     end type ErrorInfo
 
     type :: Token
         character(len=100) :: lexema
-        character(len=20) :: tipo
+        character(len=25) :: tipo
         integer :: columna
         integer :: linea
     end type Token
@@ -37,16 +37,16 @@ program main
         character(len=100) :: bandera
     end type Pais
 
-    type (Pais), dimension(100) :: paises_continente
+    type (Pais), allocatable, dimension(:) :: paises_continente
 
     type :: Continente
         character(len=100) :: nombre_continente
-        type(Pais), dimension(100) :: paises_continente
+        type(Pais), allocatable, dimension(:) :: paises_continente
         integer :: saturacion_continente
     end type Continente
 
-    type(ErrorInfo), dimension(250) :: errores
-    type(Token), dimension(250) :: tokens
+    type(ErrorInfo), dimension(1000) :: errores
+    type(Token), dimension(5000) :: tokens
     type(Pais), dimension(250) :: paises
     type(Continente), dimension(250) :: continentes
 
@@ -73,6 +73,7 @@ program main
     is_bandera = .false.
     nuevo_pais = .false.
     nuevo_continente = .false.
+    allocate(paises_continente(100))
 
     do
         read(*, '(A)', IOSTAT=ios) buffer
@@ -85,6 +86,12 @@ program main
         char = contenido(puntero:puntero)
         if(ichar(char) == 10) then
             columna = 1 
+            if (lexema /= '') then
+                numErrores = numErrores + 1
+                errores(numErrores) = ErrorInfo(lexema, "Token no valido", columna, linea)
+                print *, "lexema erroneo: ", errores(numErrores)%caracter, "tipo: ", errores(numErrores)%descripcion, "columna: ", errores(numErrores)%columna, "linea: ", errores(numErrores)%linea
+                lexema = ''
+            end if
             linea = linea + 1
             puntero = puntero + 1
         elseif(ichar(char) == 9) then
@@ -157,19 +164,25 @@ program main
                     end if
 
                 elseif (any(char == S)) then
-                    if (trim(lexema) /= '') then
+                    if (trim(lexema) /= 'nombre' .and. trim(lexema) /= 'continente' .and. trim(lexema) /= 'pais' .and. trim(lexema) /= 'poblacion' .and. trim(lexema) /= 'saturacion' .and. trim(lexema) /= 'bandera') then
                         numErrores = numErrores + 1
-                        errores(numErrores) = ErrorInfo(char, "Token no válido", columna, linea)
+                        errores(numErrores) = ErrorInfo(lexema, "Token no valido", columna, linea)
                         print *, "lexema erroneo: ", errores(numErrores)%caracter, "tipo: ", errores(numErrores)%descripcion, "columna: ", errores(numErrores)%columna, "linea: ", errores(numErrores)%linea
-                        estado = 1
+                        lexema = ''
+                        lexema = trim(lexema) // char
+                        numTokens = numTokens + 1
+                        tokens(numTokens) = Token(lexema, "Dos puntos", columna, linea)
+                        print *, "lexema: ", tokens(numTokens)%lexema, "tipo: ", tokens(numTokens)%tipo, "columna: ", tokens(numTokens)%columna, "linea: ", tokens(numTokens)%linea
+                        lexema = ''
                     end if
                     estado = 1
                 else
                     numErrores = numErrores + 1
                     errores(numErrores) = ErrorInfo(char, "Caracter no perteneciente al alfabeto del lenguaje", columna, linea)
                     print *, "lexema erroneo: ", errores(numErrores)%caracter, "tipo: ", errores(numErrores)%descripcion, "columna: ", errores(numErrores)%columna, "linea: ", errores(numErrores)%linea
+                    lexema = trim(lexema) // char
+                    print *, "lexema actual: ", lexema
                     estado = 0
-                    lexema = ''
                 end if
                 columna = columna + 1
                 puntero = puntero + 1
@@ -217,8 +230,14 @@ program main
                             print *, "nombre pais: ", paises_continente(numPaises_continente)%nombre_pais, "poblacion: ", paises_continente(numPaises_continente)%poblacion, "saturacion: ", paises_continente(numPaises_continente)%saturacion_pais_int, "bandera: ", paises_continente(numPaises_continente)%bandera
                             nuevo_pais = .false.
                         elseif (nuevo_continente) then
+                            do i=1, numPaises_continente
+                                suma_saturacion = suma_saturacion + paises_continente(i)%saturacion_pais_int
+                            end do
+                            saturacion_continente = suma_saturacion / numPaises_continente
                             continentes(numContinentes) = Continente(nombre_continente, paises_continente, saturacion_continente)
                             print *, "nombre continente: ", continentes(numContinentes)%nombre_continente, "saturacion: ", continentes(numContinentes)%saturacion_continente
+                            deallocate(paises_continente)
+                            allocate(paises_continente(100))
                             numPaises_continente = 0
                             nuevo_continente = .false.
                         end if
@@ -230,6 +249,9 @@ program main
                         print *, "lexema erroneo: ", errores(numErrores)%caracter, "tipo: ", errores(numErrores)%descripcion, "columna: ", errores(numErrores)%columna, "linea: ", errores(numErrores)%linea
                         estado = 1
                     end if
+                elseif (any(ichar(char) - ichar('0') == N)) then
+                    lexema = trim(lexema) // char
+                    estado = 3
                 else
                     numErrores = numErrores + 1
                     errores(numErrores) = ErrorInfo(char, "Caracter no perteneciente al alfabeto del lenguaje", columna, linea)
@@ -266,7 +288,7 @@ program main
             case(3)
                 if (any(ichar(char) - ichar('0') == N)) then
                     lexema = trim(lexema) // char
-                elseif (char == '%' .and. is_saturacion) then
+                elseif (char == '%') then
                     saturacion_pais_int = atoi(lexema)
                     lexema = trim(lexema) // char
                     numTokens = numTokens + 1
@@ -317,51 +339,49 @@ program main
           print *, "No hay errores en el codigo."
     end if
 contains
-! Subrutina para convertir entero a cadena de texto
+
 subroutine generar_html_errores(numErrores, errores)
     implicit none
     integer, intent(in) :: numErrores
     type(ErrorInfo), intent(in) :: errores(numErrores)
     character(len=100000) :: html_content
-    character(len=100) :: str_descripcion, str_columna, str_linea,char_error
+    character(len=100) :: str_descripcion, str_columna, str_linea, str_numErrores, char_error
     integer :: file_unit, ios, i
-    ! Si hay errores, se crea el archivo HTML
-   if (numErrores > 0) then
-! Abrir el archivo para escribir
-        open(unit=file_unit, file="errores.html", status="replace", action="write", iostat=ios)
-        if (ios /= 0) then
-            print *, "Error al crear el archivo HTML."
-        else
-            ! Escribir la cabecera del HTML directamente al archivo
-            write(file_unit, '(A)') '<!DOCTYPE html>' // new_line('a')
-            write(file_unit, '(A)') '<html><head><style>' // new_line('a')
-            write(file_unit, '(A)') 'table { font-family: Arial, sans-serif;'
-            write(file_unit, '(A)') 'border-collapse: collapse; width: 100%; }' // new_line('a')
-            write(file_unit, '(A)') 'td, th { border: 1px solid #dddddd; text-align: left; padding: 8px; }' // new_line('a')
-            write(file_unit, '(A)') 'tr:nth-child(even) { background-color: #f2f2f2; }' // new_line('a')
-            write(file_unit, '(A)') '</style></head><body><h2>Tabla de Errores</h2>' // new_line('a')
-            write(file_unit, '(A)') '<table><tr><th>Carácter</th><th>Descripcion'
-            write(file_unit, '(A)') '</th><th>Columna</th><th>Línea</th></tr>' // new_line('a')
-            ! Bucle para formatear cada código ASCII y cada columna
-            ! Bucle para agregar filas a la tabla
-            do i = 1, numErrores
-                write(str_descripcion, '(A)') trim(errores(i)%descripcion)
-                write(str_columna, '(I0)') errores(i)%columna
-                write(str_linea, '(I0)')  errores(i)%linea
-                write(char_error, '(A)') trim(errores(i)%caracter)
-
-                ! Escribir cada fila directamente al archivo
-                write(file_unit, '(A)') '<tr><td>' // char_error // '</td><td>' // trim(str_descripcion) // &
-                '</td><td>' // trim(str_columna) // '</td><td>'&
-                 // trim(str_linea) // '</td></tr>' // new_line('a')
-            end do
-            ! Cerrar la tabla y el HTML
-            write(file_unit, '(A)') '</table></body></html>'
-            close(file_unit)
-        end if
+    
+    ! Abrir el archivo para escribir
+    open(unit=file_unit, file="errores.html", status="replace", action="write", iostat=ios)
+    if (ios /= 0) then
+        print *, "Error al crear el archivo HTML."
     else
-        print *, "No hay errores en el código."
+        ! Escribir la cabecera del HTML directamente al archivo
+        write(file_unit, '(A)') '<!DOCTYPE html>' // new_line('a')
+        write(file_unit, '(A)') '<html><head><title>Errores encontrados en el lenguaje</title><style>' // new_line('a')
+        write(file_unit, '(A)') 'table { font-family: Arial, sans-serif;'
+        write(file_unit, '(A)') 'border-collapse: collapse; width: 100%; }' // new_line('a')
+        write(file_unit, '(A)') 'td, th { border: 1px solid #dddddd; text-align: left; padding: 8px; }' // new_line('a')
+        write(file_unit, '(A)') 'tr:nth-child(even) { background-color: #f2f2f2; }' // new_line('a')
+        write(file_unit, '(A)') '</style></head><body><h2>Errores encontrados en el lenguaje</h2>' // new_line('a')
+        write(file_unit, '(A)') '<table><tr><th>No.</th><th>Error</th><th>Descripcion'
+        write(file_unit, '(A)') '</th><th>Fila</th><th>Columna</th></tr>' // new_line('a')
+        ! Bucle para formatear cada código ASCII y cada columna
+        ! Bucle para agregar filas a la tabla
+        do i = 1, numErrores
+            write(str_numErrores, '(I0)') i
+            write(str_descripcion, '(A)') trim(errores(i)%descripcion)
+            write(str_columna, '(I0)') errores(i)%columna
+            write(str_linea, '(I0)')  errores(i)%linea
+            write(char_error, '(A)') trim(errores(i)%caracter)
+
+            ! Escribir cada fila directamente al archivo
+            write(file_unit, '(A)') '<tr><td>' // trim(str_numErrores) // '</td><td>' // char_error // '</td><td>' // trim(str_descripcion) // &
+            '</td><td>' // trim(str_linea) // '</td><td>'&
+                // trim(str_columna) // '</td></tr>' // new_line('a')
+        end do
+        ! Cerrar la tabla y el HTML
+        write(file_unit, '(A)') '</table></body></html>'
+        close(file_unit)
     end if
+
 end subroutine generar_html_errores
 
 function itoa(num) result(str)
